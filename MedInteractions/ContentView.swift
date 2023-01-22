85a6f09b02dcbe1bb6d0c825fb1b3dbb819aa78d
joanna-stalenczyk <joanna.stalenczyk@schibsted.com>
2023-01-22 14:53:21 +0100

    first commit

---
+++ MedInteractions/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MedInteractions
+//
+//  Created by Joanna Staleńczyk on 22/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ MedInteractions/MedInteractionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedInteractionsApp.swift
+//  MedInteractions
+//
+//  Created by Joanna Staleńczyk on 22/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MedInteractionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
